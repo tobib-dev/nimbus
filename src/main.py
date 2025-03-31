efc9f830a0bdde668f31c74ac95664d70cf6d325
tobii19-py <tobi19.py@gmail.com>
2025-03-31 18:01:04 -0500

    Handle spotify callback after authentication

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 import sp_auth
 
 app = Flask(__name__)
@@ -7,9 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/login-spotify")
-def spotify():
+@app.route("/spotify-login")
+def login_spotify():
+    # Redirect to Spotify Login
     return redirect(sp_auth.get_auth_url())
+
+@app.route("/api/callback/spotify")
+def callback_spotify():
+    # Handle Spotify OAuth callback
+    code = request.args.get("code")
+    token_info = sp_auth.get_token_info(code)
+
+    sp_lib = sp_auth.extract(token_info)
+    return render_template("dashboard.html", library=sp_lib)
 
 if __name__ == "__main__":
     app.run(debug=True)
